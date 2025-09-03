da4555c0034665bf946544e411ee1a6b074ca297
Area15 R&D <muslimsoap@gmail.com>
2025-09-03 08:37:23 +0800

    Update kernel_entry.asm

--- kernel_entry.asm
+++ kernel_entry.asm
@@ -1,12 +1,35 @@
-; kernel_entry.asm
 [bits 32]
 [extern kmain]
 
 global _start
 _start:
-    ; Call the kernel main function
+    ; Set up data segments
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    
+    ; Set up stack
+    mov esp, 0x90000
+    
+    ; Clear direction flag
+    cld
+    
+    ; Initialize .bss section (zero out uninitialized data)
+    extern _bss_start
+    extern _bss_end
+    mov edi, _bss_start
+    mov ecx, _bss_end
+    sub ecx, _bss_start
+    xor eax, eax
+    rep stosb
+    
+    ; Call kernel main
     call kmain
     
-    ; Halt if kernel returns
+    ; Halt if kernel returns (should never happen)
     cli
     hlt
+    jmp $
