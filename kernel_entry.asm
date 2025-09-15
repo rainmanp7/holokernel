ba19e2b544b604e2e05a3509da67020aaff96e4f
Area15 R&D <muslimsoap@gmail.com>
2025-09-15 18:12:00 +0800

    Update kernel_entry.asm

--- kernel_entry.asm
+++ kernel_entry.asm
@@ -1,12 +1,15 @@
 ; kernel_entry.asm
+
 [bits 32]
 [extern kmain]
-
 global _start
+
+.section .note.GNU-stack,"",@progbits  ; Add this line
+
 _start:
     ; Call the kernel main function
     call kmain
-    
+
     ; Halt if kernel returns
     cli
     hlt
