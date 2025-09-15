0d94b2c2af8629d8f35381b8a7b06289c64034b4
Area15 R&D <muslimsoap@gmail.com>
2025-09-15 19:32:18 +0800

    Update kernel_entry.asm

--- kernel_entry.asm
+++ kernel_entry.asm
@@ -1,15 +1,27 @@
 ; kernel_entry.asm
-
 [bits 32]
 [extern kmain]
+
 global _start
-
-.section .note.GNU-stack,"",@progbits  ; Add this line
-
 _start:
+    ; Set up segment registers for 32-bit protected mode
+    mov ax, 0x10          ; Data segment selector from GDT
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    
+    ; Set up stack pointer
+    mov esp, 0x7c00       ; Stack below bootloader
+    
     ; Call the kernel main function
     call kmain
-
+    
     ; Halt if kernel returns
     cli
     hlt
+
+; Infinite loop as backup
+hang:
+    jmp hang
